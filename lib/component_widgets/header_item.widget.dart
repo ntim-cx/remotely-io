b2b51eb124836ca306f42c984747c2efa4130ef6
qwaiks <kwaku315@gmail.com>
2021-11-01 00:42:43 +0000

    update changes to components

---
+++ lib/component_widgets/header_item.widget.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:remotely_io/config/utils.dart';
+import 'package:remotely_io/constants/colors.dart';
+import 'package:remotely_io/constants/images.dart';
+
+class HeaderItem extends StatelessWidget {
+  final bool isVertical;
+  final String title;
+  final String subHeader;
+
+  const HeaderItem({Key key, this.isVertical, this.title, this.subHeader})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final header = Text(
+      title,
+      style: Theme.of(context).textTheme.headline6,
+    );
+
+    return isVertical
+        ? Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              header,
+              Utils.verticalSpacer(),
+              Text(
+                subHeader,
+                style: TextStyle(color: AppColors.colorGreyDeeper),
+              )
+            ],
+          )
+        : Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              header,
+              Row(
+                children: [
+                  Text("See more"),
+                  Utils.horizontalSpacer(space: 8),
+                  SvgPicture.asset(AppImages.ic_right_arrow,
+                      color: AppColors.colorGreyDeeper)
+                ],
+              )
+            ],
+          );
+  }
+}
---
+++ lib/component_widgets/product_item.widget.dart
@@ -0,0 +1,105 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:remotely_io/config/utils.dart';
+import 'package:remotely_io/constants/colors.dart';
+import 'package:remotely_io/constants/images.dart';
+
+class ProductItem extends StatelessWidget {
+  final String title;
+  final String category;
+  final double rating;
+  final double price;
+  final String featureImage;
+
+  const ProductItem(
+      {Key key,
+      this.title,
+      this.category,
+      this.rating,
+      this.price,
+      this.featureImage})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final bookmarkImage = Container(
+      padding: EdgeInsets.all(9.0),
+      decoration: BoxDecoration(shape: BoxShape.circle, color: Colors.white),
+      child: SvgPicture.asset(AppImages.ic_bookmark_heart),
+    );
+
+    final featureProductImage = Container(
+      width: 105,
+      height: 86,
+      child: Stack(
+        children: [
+          SvgPicture.asset(
+            featureImage,
+            width: 60,
+            height: 60,
+          ),
+          bookmarkImage,
+        ],
+      ),
+      decoration: BoxDecoration(
+        color: AppColors.colorWhiteShade,
+        borderRadius: BorderRadius.circular(8.0),
+      ),
+    );
+
+    final productSubtitle = Row(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: [
+        Text.rich(
+          TextSpan(
+            text: "$category",
+            children: [TextSpan(text: "$rating")],
+          ),
+        ),
+        RatingBar.builder(
+          initialRating: 1,
+          minRating: 1,
+          direction: Axis.horizontal,
+          itemCount: 1,
+          itemPadding: EdgeInsets.symmetric(horizontal: 4.0),
+          itemBuilder: (context, _) => Icon(
+            Icons.star,
+            size: 12.0,
+            color: Colors.amber,
+          ),
+          onRatingUpdate: (rating) {},
+        ),
+      ],
+    );
+
+    final productDescription = Column(
+      children: [
+        Text(title),
+        Utils.verticalSpacer(),
+        productSubtitle,
+        Utils.verticalSpacer(),
+        Text(
+          "\$$price",
+          style: Theme.of(context)
+              .textTheme
+              .headline6
+              .copyWith(color: AppColors.colorSecondary),
+        )
+      ],
+    );
+
+    return Container(
+      padding: EdgeInsets.all(8.0),
+      decoration: BoxDecoration(
+          color: Colors.white, borderRadius: BorderRadius.circular(8.0)),
+      child: Row(
+        children: [
+          featureProductImage,
+          Utils.horizontalSpacer(),
+          productDescription
+        ],
+      ),
+    );
+  }
+}
